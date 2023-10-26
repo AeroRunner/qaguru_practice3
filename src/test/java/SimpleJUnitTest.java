6012671db314819c5d20bbbc58e8373df07e19b4
AeroRunner <airplay1x6@gmail.com>
2023-10-26 20:57:54 +0300

    add new test

--- src/test/java/SimpleJUnitTest.java
+++ src/test/java/SimpleJUnitTest.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -6,13 +8,26 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SimpleJUnitTest {
+    int result;
+    @BeforeEach
+    void beforeeach(){
+        result = getResult();
+    }
 @Test
 
-void successfulSearchTest() {
-    Configuration.holdBrowserOpen = true;
-    open("https://www.google.com/");
-    $("[name=q]").setValue("selenide").pressEnter();
-    $("[id=search]").shouldHave(text("https://selenide.org"));
-}
+void firsttest() {
+    System.out.println("###        first test");
+    Assertions.assertTrue(3>2);
 
+}
+    void twotest() {
+        System.out.println("###        two test");
+        Assertions.assertTrue(3>1);
+
+    }
+    void freesttest() {
+        System.out.println("###        free test");
+        Assertions.assertTrue(3>1);
+
+    }
 }
