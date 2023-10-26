1b9067632689733b6c499a6d0f0e6b27b1d4d921
AeroRunner <airplay1x6@gmail.com>
2023-10-26 21:57:16 +0300

    add text test 2

--- src/test/java/SimpleJUnitTest.java
+++ src/test/java/SimpleJUnitTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.name;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -10,6 +11,7 @@ public class SimpleJUnitTest {
     @BeforeAll
     static void beforeAll(){
         Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
 
     }
     @Test
@@ -17,12 +19,16 @@ public class SimpleJUnitTest {
     void fillFormTest() {
 
         Configuration.holdBrowserOpen = true;
-        open("https://demoqa.com/text-box");
+        open("/text-box");
         $("#userName").setValue("Any name");
         $("#userEmail").setValue("airplay1x6@gmail.com");
         $("#currentAddress").setValue("Any address");
         $("#permanentAddress").setValue("Any per address");
         $("#submit").click();
-     //   $("[id=search]").shouldHave(text("https://selenide.org"));
+
+        $("#output").$("#name").shouldHave(text("Any name"));
+        $("#output").$("#email").shouldHave(text("airplay1x6@gmail.com"));
+        $("#output").$("#currentAddress").shouldHave(text("Any address"));
+        $("#output").$("#permanentAddress").shouldHave(text("Any per address"));
     }
 }
